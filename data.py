ad7b9c01f12a3fe0f43a32e27136fb7f61e1ec3c
madzne <matheo.jdz@gmail.com>
2024-09-30 11:33:13 +0200

    new upddate of date*
    adding new maj for data , debugging last update.
    Adding for a new

--- data.py
+++ data.py
@@ -1,1 +1,6 @@
-data
+bin_valid_chars = ["0","1"]
+dec_valid_chars = bin_valid_chars + ["2","3","4","5","6","7","8","9"]
+hex_valid_chars = dec_valid_chars + ["A","B","C","D","E","F","a","b","c","d","e","f"]
+
+ask_for_the_init_number_text = "File moi le nombre : "
+ask_again_for_the_init_number_text = "File moi encore le nombre : "
