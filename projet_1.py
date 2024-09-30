99ca57c1664daba0e453e351893ac64547810bb0
amourjunior <cyprienamour08@gmail.com>
2024-09-30 11:55:51 +0200

    Update projet_1.py

--- projet_1.py
+++ projet_1.py
@@ -33,23 +33,5 @@ def do_the_job ():
                                     init_base, \
                                     target_base)
 
-do_the_job ()
- jkhfpgn
- duk
- ryuk
- dk,
- yfukry
- utyhktyjr
- nfkrejgnv
- ergjkvnrekoejvn
- dfneknfkegjergerthtyj
- tykrtykgfhkrtyuty
- rtrhjr cdjujrdfggh
- tyjrkirjrudmde,nfkrejgnvjsfkf
 
- rfjdfh
- Dr-kngsf_fgnerggsçs
- def je_suis_uen_carrotte():
-    if a = 3
-    v = 4 + a
 
