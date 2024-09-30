24d645684b34fd4b3512133abcc9d16e3be3da03
madzne <matheo.jdz@gmail.com>
2024-09-30 11:28:27 +0200

    new update of project_1
    work in progress ;)

--- projet_1.py
+++ projet_1.py
@@ -4,9 +4,26 @@ def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
     return target_number
 
 from tools import *
+from data import *
 
 assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
 
+def is_a_valid_number (init_number):
+    i = 0
+    char_is_valid = True
+    while char_is_valid:
+        char_is_valid = init_number [i] in hex_valid_chars  #for char in init_number:
+        i = i + 1
+    return char_is_valid
+
+def ask_for_the_init_number ():
+    init_number = input (ask_for_the_init_number_text)
+    while is_a_valid_number (init_number) == False:
+        init_number = input (ask_again_for_the_init_number_text)
+    return init_number
+
+def ask_for_the_init_base ():
+    
 def do_the_job ():
     init_number = ask_for_the_init_number ()
     init_base = ask_for_the_init_base ()
