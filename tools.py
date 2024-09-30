29a1a100ad417b4edf5eb74772ccdb5f6860781e
madzne <matheo.jdz@gmail.com>
2024-09-30 12:03:23 +0200

    Update tools.py
    tools

--- tools.py
+++ tools.py
@@ -1,3 +1,17 @@
-bin_valid_chars=["0","1"]
-dec_valid_chars= bin_valid_chars +["2","3","4","5","6","7","8","9"]
-hex_valid_chars= dec_valid_chars +["A","B","C","D","E","F","a","b","c","d","e","f","f"]
+
+
+
+
+
+
+
+
+
+
+def ask_for_the_init_number ():
+    pass
+def ask_for_the_init_base ():
+    pass
+def ask_for_the_target_base ():
+    pass
+
