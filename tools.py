37c334442ad256772fd01c2701f77cd0107b5821
Julian bdt <bidaultjulian@gmail.com>
2024-09-30 10:30:22 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,2 +1,3 @@
-"test1"
-
+bin_valid_chars=["0","1"]
+dec_valid_chars= bin_valid_chars +["2","3","4","5","6","7","8","9"]
+hex_valid_chars= dec_valid_chars +["A","B","C","D","E","F";"a","b","c","d","e","f","f"]
