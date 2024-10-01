b75f7fb7ca4701dce2635c6fddb681365639087e
madzne <matheo.jdz@gmail.com>
2024-10-01 09:53:23 +0200

    data and projet_1 update

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 bin_valid_chars = ["0","1"]
 dec_valid_chars = bin_valid_chars + ["2","3","4","5","6","7","8","9"]
 hex_valid_chars = dec_valid_chars + ["A","B","C","D","E","F","a","b","c","d","e","f"]
+valid_base = ["2","10","16"]
 
 ask_for_the_init_number_text = "donne moi le nombre : "
 ask_again_for_the_init_number_text = "c'est incorecte donne moi encore le nombre : "
